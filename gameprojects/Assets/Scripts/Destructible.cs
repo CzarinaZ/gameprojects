a07c78f2efcde61c2252a8ee6a8752f50adcc858
CzarinaZ <barengczarina31@gmail.com>
2026-02-02 15:20:07 -0800

    Part 13: Added Destructible Objects

---
+++ gameprojects/Assets/Scripts/Destructible.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class Destructible : MonoBehaviour
+{
+    [SerializeField] private GameObject destroyVFX;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<DamageSource>())
+        {
+            Instantiate(destroyVFX, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
