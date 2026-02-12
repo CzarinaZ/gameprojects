2c18ffe9004621e4197d00ab039f3f5d8b97b640
CzarinaZ <barengczarina31@gmail.com>
2026-02-12 15:08:35 -0800

    Enemy Pathfinding

---
+++ gameprojects/Assets/Scripts/Enemies/AIChase.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AIChase : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+    public float distanceBetween;
+
+    private float distance;
+
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+
+        if (distance < distanceBetween)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        }
+    }
+}
